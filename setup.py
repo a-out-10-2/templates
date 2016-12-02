8cbf424837ac48350f0e27b898da3d1b30b82ed3
a_out_10_2 <moe.andrew@acm.org>
2016-12-01 23:48:01 -0600

    Adding setup.py .

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='templates',
+    version='0.0',
+    packages=['python', 'licenses', 'licenses.bin'],
+    url='http://127.0.0.1/index.html',
+    license='GPL-2.0',
+    author='a_out_10_2',
+    author_email='/dev/null',
+    description=''
+)
